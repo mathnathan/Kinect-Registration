4f21afe4e984610bb2fcb607bdffd53f8630473a
Nathan Crock <mathnathan@gmail.com>
2013-04-19 17:45:05 -0400

    Updated the README

--- README.txt
+++ README.txt
@@ -11,31 +11,21 @@ Date:    03/23/11    14:53
 
 OS SUPPORT: Only tested on Linux so far
 
-            Ubuntu
+            Ubuntu 12.04, 12.10
 
 FILES:
         KinReg.cpp - The registraion program
         CMakeLists.txt - Cmake file with build commands
-        include |
-                | libfreenect - open source driver for the kinect
+        cmake |
+              | find |
+                     | Find*.cmake all of the cmake files to find the
+                     | dependencies
 
 
-DEPENDENCIES:   OpenCV
+DEPENDENCIES:   Freenect
+                OpenCV
                 OpenGL
                 GLUT
-
-    DISCLAIMER:
-
-		I've bundled a version of the libfreenect driver into this package. Get
-		it straight from git if you want all their samples and goodies. 
-
-            libfreenect: https://github.com/OpenKinect/libfreenect
-
-        Also check out their wiki to learn about the organization
-        that put it together.
-
-            OpenKinect: http://openkinect.org/wiki/Main_Page 
-
 
 COMPILATION INSTRUCTIONS: 
 
@@ -53,8 +43,8 @@ COMPILATION INSTRUCTIONS:
     make
 
 	NOTE: If you have the dependencies installed (to your root directories, or
-	their paths in your ldpaths), then it should "just work" in UBUNTU 10.04 and
-	10.10. Please let me know otherwise
+	their paths in your ldpaths), then it should "just work" in UBUNTU 12.04 and
+	12.10. Please let me know otherwise
 
 
 ---------------------------Simple Project Manual--------------------------------------
@@ -71,14 +61,14 @@ COMPILATION INSTRUCTIONS:
 
         Once KinReg is running, a window pops up containing both images side by side.
 
-	All you do is click on pixels in both cameras the correspond to the same
+	All you do is click on pixels in both cameras which correspond to the same
 	point in world coordinates. Currently the process is rough, it can be
 	improved in the future by drawing circles around the points you click on
 	and adding colors to signify if the correspondence is accepted or not. If
 	both depth measurements for your clicks are found it stores the
 	correspondence. 
 	
-		If at least one of the corespondences is bad it deletes the match.
+		If at least one of the corespondences is bad (no depth) it deletes the match.
 
 	Once you've collected around 10 or so correspondences pushing 'p' applies 
 	procrustes analysis on the correspondences and calculates the transformation 
--- kinReg.cpp
+++ kinReg.cpp
@@ -697,4 +697,95 @@ Vec3f transformPoint( const Vec3f& pt ) {
     printf("\n----- LEAVING transformPoint() -------\n\n");
     return transformedPoint;
 
+float flann_knn(Mat& m_destinations, Mat& m_object, vector<int>& ptpairs, vector<float>& dists = vector<float>()) {
+    // find nearest neighbors using FLANN
+    Mat m_indices(m_object.rows, 1, CV_32S);
+    Mat m_dists(m_object.rows, 1, CV_32F);
+ 
+    Mat dest_32f; m_destinations.convertTo(dest_32f,CV_32FC2);
+    Mat obj_32f; m_object.convertTo(obj_32f,CV_32FC2);
+ 
+    assert(dest_32f.type() == CV_32F);
+ 
+    cv::flann::Index flann_index(dest_32f, cv::flann::KDTreeIndexParams(2));  // using 2 randomized kdtrees
+    flann_index.knnSearch(obj_32f, m_indices, m_dists, 1, cv::flann::SearchParams(64) ); 
+ 
+    int* indices_ptr = m_indices.ptr<int>(0);
+    //float* dists_ptr = m_dists.ptr<float>(0);
+    for (int i=0;i<m_indices.rows;++i) {
+        ptpairs.push_back(indices_ptr[i]);
+    }
+ 
+    dists.resize(m_dists.rows);
+    m_dists.copyTo(Mat(dists));
+ 
+    return cv::sum(m_dists)[0];
+}
+
+void findBestReansformSVD(Mat& _m, Mat& _d) {
+    Mat m; _m.convertTo(m,CV_32F);
+    Mat d; _d.convertTo(d,CV_32F);
+ 
+    Scalar d_bar = mean(d);
+    Scalar m_bar = mean(m);
+    Mat mc = m - m_bar;
+    Mat dc = d - d_bar;
+ 
+    mc = mc.reshape(1); dc = dc.reshape(1);
+     
+    Mat H(2,2,CV_32FC1);
+    for(int i=0;i<mc.rows;i++) {
+        Mat mci = mc(Range(i,i+1),Range(0,2));
+        Mat dci = dc(Range(i,i+1),Range(0,2));
+        H = H + mci.t() * dci;
+    }
+ 
+    cv::SVD svd(H);
+ 
+    Mat R = svd.vt.t() * svd.u.t();
+    double det_R = cv::determinant(R);
+    if(abs(det_R + 1.0) < 0.0001) {
+        float _tmp[4] = {1,0,0,cv::determinant(svd.vt*svd.u)};
+        R = svd.u * Mat(2,2,CV_32FC1,_tmp) * svd.vt;
+    }
+    float* _R = R.ptr<float>(0);
+    Scalar T(d_bar[0] - (m_bar[0]*_R[0] + m_bar[1]*_R[1]),d_bar[1] - (m_bar[0]*_R[2] + m_bar[1]*_R[3]));
+ 
+    m = m.reshape(1);
+    m = m * R;
+    m = m.reshape(2);
+    m = m + T;// + m_bar;
+    m.convertTo(_m,CV_32S);
+}
+
+void icp() {
+    vector<int> pair;
+    vector<float> dists;
+
+    while(true) {
+        pair.clear(); dists.clear();
+        double dist = flann_knn(destination, X, pair, dists);
+     
+        if(lastDist <= dist) {
+            X = lastGood;
+            break;  //converged?
+        }
+        lastDist = dist;
+        X.copyTo(lastGood);
+     
+        cout << "distance: " << dist << endl;
+     
+        Mat X_bar(X.size(),X.type());
+        for(int i=0;i<X.rows;i++) {
+            Point p = destination.at<Point>(pair[i],0);
+            X_bar.at<Point>(i,0) = p;
+        }
+     
+        ShowQuery(destination,X,X_bar);
+     
+        X = X.reshape(2);
+        X_bar = X_bar.reshape(2);
+        findBestReansformSVD(X,X_bar);
+        X = X.reshape(1); // back to 1-channel
+    }
 }
